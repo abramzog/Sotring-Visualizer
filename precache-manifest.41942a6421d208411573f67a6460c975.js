e7f4f1e6d06b07caa072c21abcea1c59eab42753
Abramzog <gabiabramzon@gmail.com>
2020-06-06 13:26:10 +0300

    Updates

---
+++ precache-manifest.41942a6421d208411573f67a6460c975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b313c1fcd1efc6787f158d680f56d4",
+    "url": "/Sotring-Visualizer/index.html"
+  },
+  {
+    "revision": "bc6561d4532212465bb9",
+    "url": "/Sotring-Visualizer/static/css/main.006436bb.chunk.css"
+  },
+  {
+    "revision": "a413d96b19ee2c81b4ec",
+    "url": "/Sotring-Visualizer/static/js/2.dee44989.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sotring-Visualizer/static/js/2.dee44989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6561d4532212465bb9",
+    "url": "/Sotring-Visualizer/static/js/main.802a72e8.chunk.js"
+  },
+  {
+    "revision": "b83936bcbbdcfac23843",
+    "url": "/Sotring-Visualizer/static/js/runtime-main.0131f389.js"
+  }
+]);
